9d0d200300953060fd134f312169132eba0fe461
tknmeeee <mjd.torikusai@gmail.com>
2020-02-10 18:12:59 +0900

    admin_orders_view

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -2,9 +2,11 @@ class Admins::OrdersController < ApplicationController
 	before_action :header_login_check
 	before_action :authenticate_admin!
   def show
+  	@order = Order.find(params[:id])
   end
 
   def index
+  	@orders = Order.page(params[:page]).reverse_order
   end
 
   def update
