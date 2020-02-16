fdd579ec960baeeef65bd7cd22fe7ebf77ed0cf2
ryoya1122 <koba.jht@gmail.com>
2020-02-16 21:12:47 +0900

    iroiro

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
 	belongs_to :category
 	has_many :carts, dependent: :destroy
 	has_many :orders, through: :order_details, source: :order
-
+	validates :price, numericality: { only_integer: true, greater_than: 0}
 	def self.search(search)
       return Item.all unless search
       Item.where(['name LIKE ?', "%#{search}%"])
