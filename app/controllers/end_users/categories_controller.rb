71b1639b4f9cd98abb05dd79d417d11566ba7b66
hiroki369 <kissjoel676@gmail.com>
2020-02-18 14:49:15 +0900

    make pagination

--- app/controllers/end_users/categories_controller.rb
+++ app/controllers/end_users/categories_controller.rb
@@ -2,7 +2,8 @@ class EndUsers::CategoriesController < ApplicationController
  before_action :header_login_check
   def show
   	  # category_id = params[:id]
-  	  @items = Item.where(category_id: params[:id])
+  	  @items = Item.where(category_id: params[:id]).page(params[:page])
+  	  @items_count = Item.where(category_id: params[:id])
   	  @category = Category.find(params[:id])
   	  @categories = Category.where(available: true)
   end
--- app/controllers/end_users/top_controller.rb
+++ app/controllers/end_users/top_controller.rb
@@ -2,7 +2,8 @@ class EndUsers::TopController < ApplicationController
 	before_action :header_login_check
 	def index
       @end_user = current_end_user
-      @items = Item.all.order(sort_item: "DESC")
+      @items = Item.all.order(sort_item: "DESC").page(params[:page])
+      @items_count = Item.all
       @categories = Category.where(available: true)
 	end
 
